253d34f0a81a6633edc9041366724f85f72286bb
Alexander von Weiss <alexander.weiss@rebuy.com>
2022-05-27 20:02:55 +0200

    add 3 routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {RouteAComponent} from 'src/app/route-a/route-a.component';
+import {RouteBComponent} from 'src/app/route-b/route-b.component';
+import {RouteCComponent} from 'src/app/route-c/route-c.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'a',
+        component: RouteAComponent,
+    },
+    {
+        path: 'b',
+        component: RouteBComponent,
+    },
+    {
+        path: 'c',
+        component: RouteCComponent,
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import { RouteAComponent } from './route-a/route-a.component';
+import { RouteBComponent } from './route-b/route-b.component';
+import { RouteCComponent } from './route-c/route-c.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, RouteAComponent, RouteBComponent, RouteCComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
